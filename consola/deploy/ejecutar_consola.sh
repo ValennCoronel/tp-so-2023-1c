ead066063a3a477c953327e9b575f3ea211f65d4
Franco Callero <fcallero@frba.utn.edu.ar>
2023-07-28 22:07:27 +0000

    modifico script para ejecutar las consolas

--- consola/deploy/ejecutar_consola.sh
+++ consola/deploy/ejecutar_consola.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH="/home/utnso/Desktop/tp-2023-1c-Grupo-SO-1/global/deploy" ./consola ./consola.config ./codigo
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+if [ $# -ne 1 ]; then
+	echo -e "${redColour}Parametro requerido${endColour}: ${yellowColour}<pathPseudocodigo>${endColour}"
+	exit 1
+fi
+
+pathPseudocodigo="$1"
+
+if [ ! -f "$pathPseudocodigo" ]; then
+	echo -e "${redColour}No existe el pseudocodigo o esta mal escrito la ruta al pseudocodigo${endColour}"
+	exit 1
+
+fi
+
+LD_LIBRARY_PATH="/home/utnso/Desktop/tp-2023-1c-Grupo-SO-1/global/deploy" ./consola ./consola.config "${pathPseudocodigo}"
+
--- consola/deploy/ejecutar_consola4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH="/home/utnso/Desktop/tp-2023-1c-Grupo-SO-1/global/deploy" ./consola ./consola.config ./codigo4
